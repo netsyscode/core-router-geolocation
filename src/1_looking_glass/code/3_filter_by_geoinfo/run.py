e1044dfbdd9d07b5b5feb01aa589b57eb5ac3b11
weiyz23 <272068431@qq.com>
2024-02-28 17:47:30 +0800

    [feat] Add abbr recognition for better geolocating LG servers.

--- src/1_looking_glass/code/3_filter_by_geoinfo/run.py
+++ src/1_looking_glass/code/3_filter_by_geoinfo/run.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import re
 from string import digits
 from typing import List, Set, Tuple
 
@@ -28,7 +29,7 @@ list_good_routers = []
 list_bad_routers = []
 list_tbd_routers = []
 
-def split_word(name):
+def split_word(name: str):
     name_split = set()
 
     # remove digits
@@ -73,82 +74,100 @@ def check_if_abbr(word: str, place: str) -> bool:
         elif place[j] not in vowels:
             k += 1
         j += 1
-    return ((i == len(word)) and k <= 1)
+    check_res = (i == len(word)) and (k <= 1)
+    return check_res
 
-# get candidates of abbreviation
-# need further validation for true result
+# using depth-first search to check if the word is abbreviation of a place with space
+# for those with space: 
+# 1. the first letter of slice must be the first one appear in the word for each slice
+# 2. all the letters of abbreviation must be in order, but can be missed
+# 3. if current slice can not match the abbreviation, then move to next slice
+# e.g. saint petersburg -> sp / stp / sntp / spb are all valid abbreviation
+def check_if_abbr_with_space(word: str, slice_list: list[str], idx: int, jdx: int, kdx: int) -> bool:
+    # idx for word char index, jdx for slice index, kdx for slice char index
+    # fully matched
+    if idx == len(word):
+        return True
+    # totally unmatched
+    if jdx == len(slice_list):
+        return False
+    # the first letter must be the same
+    if kdx == 0:
+        if word[idx] != slice_list[jdx][0]:
+            return False
+        else:
+            return check_if_abbr_with_space(word, slice_list, idx + 1, jdx, kdx + 1)
+    while kdx < len(slice_list[jdx]):
+        # try to match the abbreviation
+        if word[idx] == slice_list[jdx][kdx]:
+            if check_if_abbr_with_space(word, slice_list, idx + 1, jdx, kdx + 1):
+                return True
+        kdx += 1
+    # if not finished yet, means the abbreviation is not matched in current slice
+    # idx cannot over the length of word here, because every match will lead to a func call
+    return check_if_abbr_with_space(word, slice_list, idx, jdx + 1, 0)
+
+# get candidates of abbreviation, need further validation for true result
+# **the abbr format is different when the city name has space**
 def get_abbr_candidate(word : Set[str]) -> List:
     # if len(w) < 3, check if it is a contry code, filter out of these words
     word_short = [w for w in word if len(w) == 2]
-    word_long = [w for w in word if len(w) >= 3]    
-    # find all country code
-    # TODO: what if there are multiple country code?
+    word_long = [w for w in word if len(w) >= 3]
+    # TODO: what if there are combined word with country code + city name ???
     poss_country_code = []
     for w in word_short:
         if w in dict_city_by_country.keys():
             poss_country_code.append(w)
-    poss_cities = [list(dict_city_by_country[code].keys()) for code in poss_country_code]
-    # TODO: here we need to check city name with space for better result
-    # TODO: using NLTK to split the word and extract the abbreviation
-    poss_candidates = [[] for _ in range(len(word_short))]
+    # here we need to check city name with space for better result
+    poss_cities = [city for code in poss_country_code for city in list(dict_city_by_country[code].keys())]
+    poss_cities_with_space: list[str] = [key for key, value in dict_hasspace_city.items() if value[1] in poss_country_code]
+    poss_candidates = []
     
     if len(poss_cities) == 0 or len(word_long) == 0:
         return None
     
-    country_count = [0 for _ in range(len(poss_country_code))]
+    # abbr by total word
     # one abbreviation may have multiple candidates
-    # only the most chosen country is selected
+    # only the most chosen results are selected
+    # then we need to check the final result by manual
     for w in word_long:
-        for idx, cities in enumerate(poss_cities):
-            has_result = False
-            for name in cities:
-                if check_if_abbr(w, name):
-                    poss_candidates[idx].append(name)
-                    has_result = True
-            if has_result:
-                country_count[idx] += 1
-    # choose the country with most candidates
-    raw_candidates = poss_candidates[country_count.index(max(country_count))]
+        for city in poss_cities:
+            if check_if_abbr(w, city):
+                poss_candidates.append(city)
+        for city in poss_cities_with_space:
+            slice_list = re.split(r'\s+', city)
+            if check_if_abbr_with_space(w, slice_list, 0, 0, 0):
+                poss_candidates.append(dict_hasspace_city[city][3])
+
+    # bad process, many alter names included
     # merge the candidates that indicates the same city
-    dict_candidates = {}
-    # TODO: bad process, many alter names included
-    for name in raw_candidates:
+    set_candidates = set()
+    for name in poss_candidates:
         city_name = dict_city_by_name[name][3]
         if city_name in dict_city_by_name.keys():
-            if dict_city_alter_name[city_name] in dict_candidates.keys():
-                dict_candidates[dict_city_alter_name[city_name]] += 1
-            else:
-                dict_candidates[dict_city_alter_name[city_name]] = 1
-    # candidates = [dict_city_by_name[name][2] for name in raw_candidates if dict_city_by_name[name][2] in dict_city_by_name.keys()]
+            standard_name = dict_city_alter_name[city_name]
+            set_candidates.add(standard_name)
     
-    if len(dict_candidates) == 0:
+    if len(set_candidates) == 0:
         return None
     
-    # choose the top-5 frequent city in candidates
-    set_candidates = set()
-    for _ in range(5):
-        if len(dict_candidates) == 0:
+    # choose the top-10 polulation city in candidates
+    final_candidates = set()
+    for _ in range(10):
+        if len(set_candidates) == 0:
             break
         max_name = ''
-        max_count = 0
-        for name in dict_candidates.keys():
-            if dict_candidates[name] > max_count:
+        max_pop = 0
+        for name in set_candidates:
+            pop = dict_city_by_name[name][4]
+            if pop > max_pop:
                 max_name = name
-                max_count = dict_candidates[name]
-        set_candidates.add(max_name)
-        dict_candidates.pop(max_name)
-        
-            
-    # choose the city with most population
-    max_name = ''
-    max_pop = 0
-    for name in set_candidates:
-        if dict_city_by_name[name][4] > max_pop:
-            max_name = name
-            max_pop = dict_city_by_name[name][4]
+                max_pop = pop
+        final_candidates.add(max_name)
+        set_candidates.remove(max_name)
             
     # TODO: verify the result using ping or traceroute
-    return dict_city_by_name[max_name] 
+    return list(final_candidates)
 
 def check_raw_word(raw_word, iata_code=True):
     raw_word = raw_word.lower()
@@ -185,16 +204,17 @@ def check_raw_word(raw_word, iata_code=True):
     # 如果有多个候选，那么看一下他们的坐标是否相近
     if len(candidates) > 0:
         coor_candidates = [x[0][0] for x in candidates]
-        for c1 in coor_candidates:
-            for c2 in coor_candidates:
-                if abs(c1[0] - c2[0]) > 1 or abs(c1[1] - c2[1]) > 1:
-                    return ('TBD', candidates)
+        for cdx_1 in range(0, len(coor_candidates)):
+            for cdx_2 in range(cdx_1 + 1, len(coor_candidates)):
+                if abs(coor_candidates[cdx_1][0] - coor_candidates[cdx_2][0]) > 0.5 or \
+                    abs(coor_candidates[cdx_1][1] - coor_candidates[cdx_2][1]) > 0.5:
+                    return ('TBD', ('near', candidates))
         return ('GOOD', candidates[0])
     # if not matched, check if the word is abbreviation
     elif len(candidates) == 0:
-        candidate = get_abbr_candidate(set_oneword)
-        if candidate:
-            return ('GOOD', (candidate, 'abbr'))
+        candidates = get_abbr_candidate(set_oneword)
+        if candidates:
+            return ('TBD', ('abbr', candidates))
     return ('BAD', None)
 
 def check_geoinfo(geohint):
@@ -214,8 +234,9 @@ for idx in range(TEMPLATE_NUM):
         result = check_geoinfo(one_router['geohint'])
         if result[0] == 'BAD':
             list_bad_routers.append(one_router)
-        elif result[0] == 'TBD' or result[1][1] == 'abbr':
-            one_router['candiates'] = result[1]
+        elif result[0] == 'TBD':
+            one_router['candiates'] = result[1][1]
+            one_router['type'] = result[1][0]
             list_tbd_routers.append(one_router)
         elif result[0] == 'GOOD':
             city_info = result[1][0]
